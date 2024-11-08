747d35e338e8461593c5ddc3cc3d5aed02022a7e
RRAJ644 <riturajsingh.17248@gmail.com>
2024-11-08 15:41:53 +0530

    function added

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 /** @format */
 
 let arr = [
-  { id: 1, name: "john", age: "18", profession: "developer" },
-  { id: 2, name: "jack", age: "20", profession: "developer" },
-  { id: 3, name: "karen", age: "19", profession: "admin" },
-];
+  { id: 1, name: 'john', age: '18', profession: 'developer' },
+  { id: 2, name: 'jack', age: '20', profession: 'developer' },
+  { id: 3, name: 'karen', age: '19', profession: 'admin' },
+]
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map(
+    (item) =>
+      item.profession === 'developer' &&
+      console.log(
+        `id : ${item.id}, Name : ${item.name}, Age : ${item.age}, Profession : ${item.profession}`
+      )
+  )
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach((item) => {
+    item.profession === 'developer' &&
+      console.log(
+        `id : ${item.id}, Name : ${item.name}, Age : ${item.age}, Profession : ${item.profession}`
+      )
+  })
 }
 
 function addData() {
-  //Write your code here, just console.log
+  const newEmployee = { id: 4, name: 'susan', age: '20', profession: 'intern' }
+  arr.push(newEmployee)
+  console.log(arr)
 }
 
 function removeAdmin() {
-  //Write your code here, just console.log
+  const filteredArr = arr.filter((item) => item.profession !== 'admin')
+  console.log(filteredArr)
 }
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  const newArray = [
+    { id: 4, name: 'alice', age: '25', profession: 'developer' },
+    { id: 5, name: 'bob', age: '22', profession: 'developer' },
+    { id: 6, name: 'lisa', age: '30', profession: 'admin' },
+  ]
+  const concatenatedArr = arr.concat(newArray)
+  console.log(concatenatedArr)
 }
